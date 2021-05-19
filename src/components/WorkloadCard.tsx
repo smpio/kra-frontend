c8260293d37b1be5288c6de50566263a9e9242b7
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-05-19 12:31:53 +0300

    load stats only if card is visible on screen

--- src/components/WorkloadCard.tsx
+++ src/components/WorkloadCard.tsx
@@ -3,6 +3,7 @@ import API from 'api';
 import {Workload, WorkloadStats} from 'types';
 import ContainerCard from './ContainerCard';
 import styles from './WorkloadCard.module.css';
+import { useInView } from 'react-intersection-observer';
 
 interface WorkloadCardProps {
   workload: Workload;
@@ -10,15 +11,18 @@ interface WorkloadCardProps {
 
 export default function WorkloadCard(props: WorkloadCardProps) {
   const [stats, setStats] = React.useState<WorkloadStats>();
+  const {ref, inView} = useInView();
 
-  React.useState(async () => {
-    let chartStepSec = 2717;  // TODO: calc from width
-    let workload = await API.fetch(`workloads/${props.workload.id}?stats&step=${chartStepSec}`).then(r => r.json());
-    setStats(workload.stats);
-  });
+  if (!stats && inView) {
+    (async () => {
+      let chartStepSec = 2717;  // TODO: calc from width
+      let workload = await API.fetch(`workloads/${props.workload.id}?stats&step=${chartStepSec}`).then(r => r.json());
+      setStats(workload.stats);
+    })();
+  }
 
   return (
-    <div className={styles.card}>
+    <div ref={ref} className={styles.card}>
       <h2>{props.workload.kind} {props.workload.namespace}/{props.workload.name}</h2>
       {stats && Object.entries(stats).map(([containerName, containerStats]) => (
         <ContainerCard
