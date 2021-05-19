a475b54bc9e6c76a37d2a85f0540077a61281075
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-05-19 15:53:09 +0300

    eleminate double load

--- src/components/WorkloadCard.tsx
+++ src/components/WorkloadCard.tsx
@@ -11,9 +11,18 @@ interface WorkloadCardProps {
 
 export default function WorkloadCard(props: WorkloadCardProps) {
   const [stats, setStats] = React.useState<WorkloadStats>();
+  const [shouldLoad, setShouldLoad] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
   const {ref, inView} = useInView();
 
-  if (!stats && inView) {
+  if (inView && !shouldLoad) {
+    setShouldLoad(true);
+  }
+
+  React.useEffect(() => {
+    if (!shouldLoad || isLoading) return;
+    setIsLoading(true);
+
     (async () => {
       let workload = await API.getWorkload(props.workload.id, {
         stats: true,
@@ -21,7 +30,7 @@ export default function WorkloadCard(props: WorkloadCardProps) {
       });
       setStats(workload.stats);
     })();
-  }
+  }, [shouldLoad, isLoading, props.workload]);
 
   return (
     <div ref={ref} className={styles.card}>
