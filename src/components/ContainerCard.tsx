5c9dd2c37569006a0cdef7ac813df12fb76913e0
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-05-19 12:18:28 +0300

    handle empty stats

--- src/components/ContainerCard.tsx
+++ src/components/ContainerCard.tsx
@@ -8,22 +8,33 @@ interface ContainerCardProps {
 };
 
 export default function ContainerCard(props: ContainerCardProps) {
-  let memMin = math.min(props.stats.usage.map(u => u.memory_mi));
-  let memMax = math.max(props.stats.usage.map(u => u.memory_mi));
-  let memMean = math.mean(props.stats.usage.map(u => u.memory_mi));
-  let memStdDev = math.stdDev(props.stats.usage.map(u => u.memory_mi));
-  let memStdDevPercent = memStdDev / memMean * 100;
-  let memLimit = props.stats.requests[props.stats.requests.length-1].memory_limit_mi;
+  let stats = null;
+  if (props.stats.usage.length > 0) {
+    stats = {
+      memMin: math.min(props.stats.usage.map(u => u.memory_mi)),
+      memMax: math.max(props.stats.usage.map(u => u.memory_mi)),
+      memMean: math.mean(props.stats.usage.map(u => u.memory_mi)),
+      memStdDev: math.stdDev(props.stats.usage.map(u => u.memory_mi)),
+      memStdDevPercent: 0,
+      memLimit: null as number|null,
+    };
+    stats.memStdDevPercent = stats.memStdDev / stats.memMean * 100;
+    if (props.stats.requests.length > 0) {
+      stats.memLimit = props.stats.requests[props.stats.requests.length-1].memory_limit_mi;
+    }
+  }
 
   return (
     <div>
       <h3>{props.name}</h3>
-      <div>
-        Memory min: {memMin} Mi<br/>
-        Memory max: {memMax} Mi<br/>
-        Memory stdDev: {memStdDev.toFixed(0)} Mi ({memStdDevPercent.toFixed(2)}%)<br/>
-        Memory limit: {memLimit} Mi<br/>
-      </div>
+      {stats && (
+        <div>
+          Memory min: {stats.memMin} Mi<br/>
+          Memory max: {stats.memMax} Mi<br/>
+          Memory stdDev: {stats.memStdDev.toFixed(0)} Mi ({stats.memStdDevPercent.toFixed(2)}%)<br/>
+          Memory limit: {stats.memLimit} Mi<br/>
+        </div>
+      )}
       <MemoryChart stats={props.stats} />
     </div>
   );
