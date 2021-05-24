dd07429d5222473cbff581d731bced82316e3378
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-05-24 19:36:56 +0300

    add missing semicolon

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {QueryClient, QueryClientProvider} from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
