2c88929e7da6b06f6f49b89640f8224438206465
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-05-27 03:53:06 +0300

    ContainerCard: use summary

--- src/components/ContainerCard.tsx
+++ src/components/ContainerCard.tsx
@@ -19,33 +19,27 @@ export default function ContainerCard(props: ContainerCardProps) {
   if (usage.length > 0) {
     mem = {
       min: math.min(usage.map(u => u.memory_mi)),
-      max: math.max(usage.map(u => u.memory_mi)),
+      max: props.summary.max_memory_mi,
       mean: math.mean(usage.map(u => u.memory_mi)),
       stdDev: math.stdDev(usage.map(u => u.memory_mi)),
       stdDevPercent: 0,
-      limit: null as number|null,
+      limit: props.summary.memory_limit_mi,
     };
 
     mem.stdDevPercent = mem.stdDev / mem.mean * 100;
-    if (props.stats.requests.length > 0) {
-      mem.limit = props.stats.requests[props.stats.requests.length-1].memory_limit_mi;
-    }
 
     let noNaN = props.stats.usage.filter(u => !isNaN(u.cpu_m));
     if (noNaN.length > 0) {
       cpu = {
         min: math.min(noNaN.map(u => u.cpu_m)),
         max: math.max(noNaN.map(u => u.cpu_m)),
-        mean: math.mean(noNaN.map(u => u.cpu_m)),
+        mean: props.summary.avg_cpu_m,
         stdDev: math.stdDev(noNaN.map(u => u.cpu_m)),
         stdDevPercent: 0,
-        request: null as number|null,
+        request: props.summary.cpu_request_m,
       };
 
       cpu.stdDevPercent = cpu.stdDev / cpu.mean * 100;
-      if (props.stats.requests.length > 0) {
-        cpu.request = props.stats.requests[props.stats.requests.length-1].cpu_request_m;
-      }
     }
   }
 
